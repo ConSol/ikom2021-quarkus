8894ec4eec59050bad358786dfcc1344f67b7307
Marco Bungart <marco.bungart@consol.de>
2021-06-28 14:31:03 +0200

    Use path parameter

--- src/main/java/de/consol/dus/MyResource.java
+++ src/main/java/de/consol/dus/MyResource.java
@@ -2,6 +2,7 @@ package de.consol.dus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,5 +13,12 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWithParameter(@PathParam("name") String name) {
+        return String.format("Hello %s", name);
     }
 }
