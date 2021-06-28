2db70a04ba81b729d4d8cccb2d0dca8b603007b1
Marco Bungart <marco.bungart@consol.de>
2021-06-28 15:14:15 +0200

    added endpoint to post a user

--- src/main/java/de/consol/dus/MyResource.java
+++ src/main/java/de/consol/dus/MyResource.java
@@ -1,15 +1,21 @@
 package de.consol.dus;
 
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
 public class MyResource {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyResource.class);
 
     @GET
     public List<User> hello() {
@@ -22,5 +28,12 @@ public class MyResource {
     @Path("{name}")
     public User helloWithParameter(@PathParam("name") String name) {
         return User.of(name);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public User createUser(User newUser) {
+        logger.info("Received user: {}", newUser);
+        return newUser;
     }
 }
